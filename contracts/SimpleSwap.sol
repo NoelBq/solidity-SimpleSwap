60e89063136d2e1fe989ef96cc0f23c1dcc46a2a
NoelBq <noeliabaquerostaci@gmail.com>
2025-07-20 20:02:03 -0300

    addressed feedback

--- contracts/SimpleSwap.sol
+++ contracts/SimpleSwap.sol
@@ -10,28 +10,41 @@ import "@openzeppelin/contracts/utils/math/Math.sol";
  * @dev A simple liquidity pool contract for swapping between two specific tokens
  * @notice This contract allows users to add/remove liquidity and swap between two tokens
  */
+/**
+ * @title SimpleSwap
+ * @dev A simple liquidity pool contract for swapping between two specific tokens
+ * @notice This contract allows users to add/remove liquidity and swap between two tokens
+ */
 contract SimpleSwap is ReentrancyGuard {
     using Math for uint256;
 
     /// @notice The first token in the pair
+    /// @dev Immutable address of tokenA
     address public immutable tokenA;
-    
-    /// @notice The second token in the pair  
+
+    /// @notice The second token in the pair
+    /// @dev Immutable address of tokenB
     address public immutable tokenB;
-    
+
     /// @notice Reserve amount of tokenA in the pool
+    /// @dev Internal accounting for tokenA reserves
     uint256 public reserveA;
-    
+
     /// @notice Reserve amount of tokenB in the pool
+    /// @dev Internal accounting for tokenB reserves
     uint256 public reserveB;
-    
+
     /// @notice Total liquidity tokens issued
+    /// @dev Internal accounting for total supply of LP tokens
     uint256 public totalSupply;
-    
+
     /// @notice Mapping of user addresses to their liquidity token balances
+    /// @dev Internal accounting for user LP token balances
     mapping(address => uint256) public liquidityBalances;
 
-    // Custom errors to replace long strings in require statements
+    // ------------------------------------------------------------------------
+    // Custom Errors
+    // ------------------------------------------------------------------------
     error Expired();
     error IdenticalAddresses();
     error ZeroAddress();
@@ -45,11 +58,14 @@ contract SimpleSwap is ReentrancyGuard {
     error NoLiquidity();
     error InvalidPath();
 
-    /// @notice Emitted when liquidity is added to the pool
-    /// @param user Address that added liquidity
-    /// @param amountA Amount of tokenA added
-    /// @param amountB Amount of tokenB added  
-    /// @param liquidity Amount of liquidity tokens minted
+
+    /**
+     * @notice Emitted when liquidity is added to the pool
+     * @param user Address that added liquidity
+     * @param amountA Amount of tokenA added
+     * @param amountB Amount of tokenB added
+     * @param liquidity Amount of liquidity tokens minted
+     */
     event LiquidityAdded(
         address indexed user,
         uint256 amountA,
@@ -57,11 +73,13 @@ contract SimpleSwap is ReentrancyGuard {
         uint256 liquidity
     );
 
-    /// @notice Emitted when liquidity is removed from the pool
-    /// @param user Address that removed liquidity
-    /// @param amountA Amount of tokenA removed
-    /// @param amountB Amount of tokenB removed
-    /// @param liquidity Amount of liquidity tokens burned
+    /**
+     * @notice Emitted when liquidity is removed from the pool
+     * @param user Address that removed liquidity
+     * @param amountA Amount of tokenA removed
+     * @param amountB Amount of tokenB removed
+     * @param liquidity Amount of liquidity tokens burned
+     */
     event LiquidityRemoved(
         address indexed user,
         uint256 amountA,
@@ -69,12 +87,14 @@ contract SimpleSwap is ReentrancyGuard {
         uint256 liquidity
     );
 
-    /// @notice Emitted when tokens are swapped
-    /// @param user Address that performed the swap
-    /// @param tokenIn Address of input token
-    /// @param tokenOut Address of output token
-    /// @param amountIn Amount of input tokens
-    /// @param amountOut Amount of output tokens
+    /**
+     * @notice Emitted when tokens are swapped
+     * @param user Address that performed the swap
+     * @param tokenIn Address of input token
+     * @param tokenOut Address of output token
+     * @param amountIn Amount of input tokens
+     * @param amountOut Amount of output tokens
+     */
     event TokensSwapped(
         address indexed user,
         address indexed tokenIn,
@@ -83,16 +103,20 @@ contract SimpleSwap is ReentrancyGuard {
         uint256 amountOut
     );
 
-    /// @notice Modifier to check transaction deadline
-    /// @param deadline Timestamp deadline for the transaction
+    /**
+     * @notice Modifier to check transaction deadline
+     * @param deadline Timestamp deadline for the transaction
+     */
     modifier validDeadline(uint256 deadline) {
         if (block.timestamp > deadline) revert Expired();
         _;
     }
 
-    /// @notice Constructor to initialize the token pair
-    /// @param _tokenA Address of the first token
-    /// @param _tokenB Address of the second token
+    /**
+     * @notice Constructor to initialize the token pair
+     * @param _tokenA Address of the first token
+     * @param _tokenB Address of the second token
+     */
     constructor(address _tokenA, address _tokenB) {
         if (_tokenA == _tokenB) revert IdenticalAddresses();
         if (_tokenA == address(0) || _tokenB == address(0)) revert ZeroAddress();
@@ -110,23 +134,34 @@ contract SimpleSwap is ReentrancyGuard {
      * @return amountA Final amount of tokenA to add
      * @return amountB Final amount of tokenB to add
      */
+    /**
+     * @dev Calculates optimal token amounts for adding liquidity
+     * @param amountADesired Desired amount of tokenA
+     * @param amountBDesired Desired amount of tokenB
+     * @param amountAMin Minimum acceptable amount of tokenA
+     * @param amountBMin Minimum acceptable amount of tokenB
+     * @return amountA Final amount of tokenA to add
+     * @return amountB Final amount of tokenB to add
+     */
     function _calculateOptimalAmounts(
         uint256 amountADesired,
         uint256 amountBDesired,
         uint256 amountAMin,
         uint256 amountBMin
     ) internal view returns (uint256 amountA, uint256 amountB) {
-        if (reserveA == 0 && reserveB == 0) {
+        uint256 _reserveA = reserveA;
+        uint256 _reserveB = reserveB;
+        if (_reserveA == 0 && _reserveB == 0) {
             // First liquidity deposit
             (amountA, amountB) = (amountADesired, amountBDesired);
         } else {
             // Calculate optimal amounts based on current ratio
-            uint256 amountBOptimal = (amountADesired * reserveB) / reserveA;
+            uint256 amountBOptimal = (amountADesired * _reserveB) / _reserveA;
             if (amountBOptimal <= amountBDesired) {
                 if (amountBOptimal < amountBMin) revert InsufficientAmountB();
                 (amountA, amountB) = (amountADesired, amountBOptimal);
             } else {
-                uint256 amountAOptimal = (amountBDesired * reserveA) / reserveB;
+                uint256 amountAOptimal = (amountBDesired * _reserveA) / _reserveB;
                 assert(amountAOptimal <= amountADesired);
                 if (amountAOptimal < amountAMin) revert InsufficientAmountA();
                 (amountA, amountB) = (amountAOptimal, amountBDesired);
@@ -136,6 +171,19 @@ contract SimpleSwap is ReentrancyGuard {
 
     /**
      * @notice Add liquidity to the pool
+     * @param amountADesired Desired amount of tokenA
+     * @param amountBDesired Desired amount of tokenB
+     * @param amountAMin Minimum acceptable amount of tokenA
+     * @param amountBMin Minimum acceptable amount of tokenB
+     * @param to Address that will receive the liquidity tokens
+     * @param deadline Timestamp limit for the transaction
+     * @return amountA Effective amount of tokenA added
+     * @return amountB Effective amount of tokenB added
+     * @return liquidity Amount of liquidity tokens issued
+     */
+    /**
+     * @notice Add liquidity to the pool
+     * @dev Transfers tokens from user and mints LP tokens
      * @param amountADesired Desired amount of tokenA
      * @param amountBDesired Desired amount of tokenB
      * @param amountAMin Minimum acceptable amount of tokenA
@@ -163,6 +211,11 @@ contract SimpleSwap is ReentrancyGuard {
     {
         if (to == address(0)) revert ZeroAddress();
         
+        // Cache state variables
+        uint256 _reserveA = reserveA;
+        uint256 _reserveB = reserveB;
+        uint256 _totalSupply = totalSupply;
+
         // Calculate optimal amounts
         (amountA, amountB) = _calculateOptimalAmounts(
             amountADesired,
@@ -170,39 +223,50 @@ contract SimpleSwap is ReentrancyGuard {
             amountAMin,
             amountBMin
         );
-        
+
         // Transfer tokens from user to contract
         IERC20(tokenA).transferFrom(msg.sender, address(this), amountA);
         IERC20(tokenB).transferFrom(msg.sender, address(this), amountB);
-        
+
         // Calculate liquidity to mint
-        if (totalSupply == 0) {
+        if (_totalSupply == 0) {
             // First time adding liquidity
             liquidity = Math.sqrt(amountA * amountB);
             if (liquidity == 0) revert InsufficientLiquidityMinted();
         } else {
-            // Liquidity proportional to existing
+            // Liquidity proporcional a la existente
             liquidity = Math.min(
-                (amountA * totalSupply) / reserveA,
-                (amountB * totalSupply) / reserveB
+                (amountA * _totalSupply) / _reserveA,
+                (amountB * _totalSupply) / _reserveB
             );
         }
-        
+
         if (liquidity == 0) revert InsufficientLiquidityMinted();
-        
+
         // Update all state variables in a single block to minimize gas costs
         unchecked {
-            reserveA += amountA;
-            reserveB += amountB;
-            totalSupply += liquidity;
+            reserveA = _reserveA + amountA;
+            reserveB = _reserveB + amountB;
+            totalSupply = _totalSupply + liquidity;
             liquidityBalances[to] += liquidity;
         }
-        
+
         emit LiquidityAdded(to, amountA, amountB, liquidity);
     }
 
     /**
      * @notice Remove liquidity from the pool
+     * @param liquidity Amount of liquidity tokens to burn
+     * @param amountAMin Minimum acceptable amount of tokenA
+     * @param amountBMin Minimum acceptable amount of tokenB
+     * @param to Address that will receive the tokens
+     * @param deadline Timestamp limit for the transaction
+     * @return amountA Amount of tokenA received
+     * @return amountB Amount of tokenB received
+     */
+    /**
+     * @notice Remove liquidity from the pool
+     * @dev Burns LP tokens and transfers underlying tokens to user
      * @param liquidity Amount of liquidity tokens to burn
      * @param amountAMin Minimum acceptable amount of tokenA
      * @param amountBMin Minimum acceptable amount of tokenB
@@ -225,35 +289,50 @@ contract SimpleSwap is ReentrancyGuard {
         validDeadline(deadline)
         returns (uint256 amountA, uint256 amountB)
     {
+        uint256 _reserveA = reserveA;
+        uint256 _reserveB = reserveB;
+        uint256 _totalSupply = totalSupply;
+        uint256 _liquidityBalance = liquidityBalances[msg.sender];
+
         if (to == address(0)) revert ZeroAddress();
         if (liquidity == 0) revert InsufficientLiquidity();
-        if (totalSupply == 0) revert NoLiquidity();
-        if (liquidityBalances[msg.sender] < liquidity) revert InsufficientLiquidityBalance();
-        
+        if (_totalSupply == 0) revert NoLiquidity();
+        if (_liquidityBalance < liquidity) revert InsufficientLiquidityBalance();
+
         // Calculate amounts to return proportionally
-        amountA = (liquidity * reserveA) / totalSupply;
-        amountB = (liquidity * reserveB) / totalSupply;
-        
+        amountA = (liquidity * _reserveA) / _totalSupply;
+        amountB = (liquidity * _reserveB) / _totalSupply;
+
         if (amountA < amountAMin) revert InsufficientAmountA();
         if (amountB < amountBMin) revert InsufficientAmountB();
-        
+
         // Update all state variables in a single block to minimize gas costs
         unchecked {
-            liquidityBalances[msg.sender] -= liquidity;
-            totalSupply -= liquidity;
-            reserveA -= amountA;
-            reserveB -= amountB;
+            liquidityBalances[msg.sender] = _liquidityBalance - liquidity;
+            totalSupply = _totalSupply - liquidity;
+            reserveA = _reserveA - amountA;
+            reserveB = _reserveB - amountB;
         }
-        
+
         // Transfer tokens to user
         IERC20(tokenA).transfer(to, amountA);
         IERC20(tokenB).transfer(to, amountB);
-        
+
         emit LiquidityRemoved(msg.sender, amountA, amountB, liquidity);
     }
 
     /**
      * @notice Swap exact amount of input tokens for output tokens
+     * @param amountIn Exact amount of input tokens
+     * @param amountOutMin Minimum acceptable amount of output tokens
+     * @param path Array with token addresses [tokenIn, tokenOut]
+     * @param to Address that will receive the output tokens
+     * @param deadline Timestamp limit for the transaction
+     * @return amounts Array with amounts [amountIn, amountOut]
+     */
+    /**
+     * @notice Swap exact amount of input tokens for output tokens
+     * @dev Transfers input tokens from user and sends output tokens
      * @param amountIn Exact amount of input tokens
      * @param amountOutMin Minimum acceptable amount of output tokens
      * @param path Array with token addresses [tokenIn, tokenOut]
@@ -273,47 +352,53 @@ contract SimpleSwap is ReentrancyGuard {
         validDeadline(deadline)
         returns (uint256[] memory amounts)
     {
+        uint256 _reserveA = reserveA;
+        uint256 _reserveB = reserveB;
+        uint256 _totalSupply = totalSupply;
+        address _tokenA = tokenA;
+        address _tokenB = tokenB;
+
         if (path.length != 2) revert InvalidPath();
         if (amountIn == 0) revert InsufficientInputAmount();
         if (to == address(0)) revert ZeroAddress();
         if (path[0] == path[1]) revert IdenticalAddresses();
-        if (totalSupply == 0) revert InsufficientLiquidity();
-        
+        if (_totalSupply == 0) revert InsufficientLiquidity();
+
         // Validate path tokens match our pair
-        if (!((path[0] == tokenA && path[1] == tokenB) || (path[0] == tokenB && path[1] == tokenA))) {
+        if (!((path[0] == _tokenA && path[1] == _tokenB) || (path[0] == _tokenB && path[1] == _tokenA))) {
             revert InvalidPath();
         }
-        
+
         // Determine input/output reserves
-        bool isTokenAInput = path[0] == tokenA;
-        uint256 reserveIn = isTokenAInput ? reserveA : reserveB;
-        uint256 reserveOut = isTokenAInput ? reserveB : reserveA;
-        
+        bool isTokenAInput = path[0] == _tokenA;
+        uint256 reserveIn = isTokenAInput ? _reserveA : _reserveB;
+        uint256 reserveOut = isTokenAInput ? _reserveB : _reserveA;
+
         // Calculate output amount
         uint256 amountOut = getAmountOut(amountIn, reserveIn, reserveOut);
         if (amountOut < amountOutMin) revert InsufficientOutputAmount();
-        
+
         amounts = new uint256[](2);
         amounts[0] = amountIn;
         amounts[1] = amountOut;
-        
+
         // Transfer input tokens from user to contract
         IERC20(path[0]).transferFrom(msg.sender, address(this), amountIn);
-        
+
         // Update reserves in a single block to minimize gas costs
         unchecked {
             if (isTokenAInput) {
-                reserveA += amountIn;
-                reserveB -= amountOut;
+                reserveA = _reserveA + amountIn;
+                reserveB = _reserveB - amountOut;
             } else {
-                reserveB += amountIn;
-                reserveA -= amountOut;
+                reserveB = _reserveB + amountIn;
+                reserveA = _reserveA - amountOut;
             }
         }
-        
+
         // Transfer output tokens to user
         IERC20(path[1]).transfer(to, amountOut);
-        
+
         emit TokensSwapped(msg.sender, path[0], path[1], amountIn, amountOut);
     }
 
@@ -321,12 +406,26 @@ contract SimpleSwap is ReentrancyGuard {
      * @notice Get the price of tokenA in terms of tokenB
      * @return price Price of tokenA in terms of tokenB (with 18 decimals)
      */
-    function getPrice(address, address) external view returns (uint256 price) {
-        if (totalSupply == 0) revert NoLiquidity();
-        if (reserveA == 0 || reserveB == 0) revert InsufficientLiquidity();
-        
+    /**
+     * @notice Get the price of tokenA in terms of tokenB
+     * @dev Returns price with 18 decimals
+     * @param tokenA_ Address of tokenA (ignored, for interface compatibility)
+     * @param tokenB_ Address of tokenB (ignored, for interface compatibility)
+     * @return price Price of tokenA in terms of tokenB (with 18 decimals)
+     */
+    /**
+     * @notice Get the price of tokenA in terms of tokenB
+     * @dev Returns price with 18 decimals
+     * @return price Price of tokenA in terms of tokenB (with 18 decimals)
+     */
+    function getPrice(address /*tokenA_*/, address /*tokenB_*/) external view returns (uint256 price) {
+        uint256 _reserveA = reserveA;
+        uint256 _reserveB = reserveB;
+        uint256 _totalSupply = totalSupply;
+        if (_totalSupply == 0) revert NoLiquidity();
+        if (_reserveA == 0 || _reserveB == 0) revert InsufficientLiquidity();
         // Price = reserveB / reserveA * 10^18 to maintain precision
-        price = (reserveB * 1e18) / reserveA;
+        price = (_reserveB * 1e18) / _reserveA;
     }
 
     /**
@@ -352,6 +451,7 @@ contract SimpleSwap is ReentrancyGuard {
 
     /**
      * @notice Get pool reserves and total supply
+     * @dev Returns reserves and total supply for analytics/UI
      * @return _reserveA Reserve of tokenA
      * @return _reserveB Reserve of tokenB
      * @return _totalSupply Total liquidity tokens issued
@@ -368,6 +468,7 @@ contract SimpleSwap is ReentrancyGuard {
 
     /**
      * @notice Get user's liquidity balance
+     * @dev Returns the LP token balance of a user
      * @param user User address
      * @return balance User's liquidity token balance
      */
